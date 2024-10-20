40d2d8eedf574e11f331507f005bd1986eb0dd3c
unknown <ehsunjalili@gmail.com>
2024-10-20 13:40:01 +0330

    Change redis config settings to deploy

--- src/redis.js
+++ src/redis.js
@@ -1,11 +1,23 @@
-const redis = require('redis');
+const Redis = require('ioredis');
 
-const redisClient = redis.createClient();
-redisClient.on('error', (err) => {
-    console.log(err);
+const redisUri = process.env.REDIS_URI;
+const redis = new Redis(redisUri);
+
+redis.on('connect', () => {
+    console.log('Connected to Redis');
 });
-redisClient.connect().then(() => {
-    console.log('Redis connected !');
+
+redis.on('error', (err) => {
+    console.error('Redis connection error:', err);
 });
+
+const redisClient = redis;
+// const redisClient = redis.createClient();
+// redisClient.on('error', (err) => {
+//     console.log(err);
+// });
+// redisClient.connect().then(() => {
+//     console.log('Redis connected !');
+// });
 
 module.exports = redisClient
