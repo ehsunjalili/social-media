6aa3ea45c995cc8a7a9a508a0638ef78b68a0b3e
unknown <ehsunjalili@gmail.com>
2024-10-20 13:05:27 +0330

    Set deployment settings

--- src/server.js
+++ src/server.js
@@ -13,7 +13,10 @@ if (!productionMode) {
 //* Database connection
 const connectToDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI);
+        await mongoose.connect(process.env.MONGO_URI,{
+            useNewUrlParser:true,
+            authSource:'admin',
+        });
         console.log(`mongoDB connected successfully : ${mongoose.connection.host}`);
     } catch (error) {
         console.error(`Error in DB connection : ${error}`);
